3b05225a6d7ed4c6ea0aa17e92b5ff607c795ee2
Javier Gago <javiergago1986@proton.me>
2024-05-18 16:54:37 +0100

    first commit

---
+++ functions/index.js
@@ -0,0 +1,42 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const axios = require("axios");
+
+admin.initializeApp();
+const db = admin.firestore();
+
+// Función para llamar a la API de Open Exchange Rates y devolver los datos
+exports.fetchExchangeRates = functions.https.onRequest(
+    async (request, response) => {
+      const apiUrl = "https://openexchangerates.org/api/latest.json";
+      const appId = functions.config().openexchange.app_id;
+      const cacheDoc = db.collection("exchangeRates").doc("latest");
+      const cacheDuration = 86400000; // 24 horas en milisegundos
+
+      try {
+        const cache = await cacheDoc.get();
+        const now = Date.now();
+
+        if (cache.exists && (now - cache.data().timestamp < cacheDuration)) {
+          console.log("Serving from cache");
+          response.status(200).send(cache.data().rates);
+        } else {
+          console.log("Fetching new data");
+          const apiResponse = await axios.get(apiUrl, {
+            params: {
+              app_id: appId,
+            },
+          });
+          const data = {
+            rates: apiResponse.data,
+            timestamp: now,
+          };
+          await cacheDoc.set(data);
+          response.status(200).send(apiResponse.data);
+        }
+      } catch (error) {
+        console.error("Error fetching data from API:", error);
+        response.status(500).send("Error fetching data from API");
+      }
+    },
+);
